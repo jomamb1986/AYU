a7fabd81310f3898e55b45d9b4494be2f46a7721
jomamb1986 <cns.oru@gmail.com>
2025-06-26 11:35:49 -0400

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# <!-- este es el codigo para app/models/__init__.py -->
-from app.models.user import User
-from app.models.cylinder import Cylinder
-from app.models.historial import HistorialMovimiento
